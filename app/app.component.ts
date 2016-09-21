2b0ae5d7f23bb376ad84c530154786d8327010ee
vaanxy <274824370@qq.com>
2016-09-22 00:43:09 +0800

    fist commit

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+import {CoursesComponent} from './courses.component'
+@Component({
+    selector: 'my-app',
+    template: '<h1>我的第一个Angular2应用呀</h1>'
+    // template: '<h1>Hello Angular2</h1><courses></courses>',
+    // directives: [CoursesComponent]
+})
+export class AppComponent { }
