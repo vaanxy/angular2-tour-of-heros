df90e62ef6f8da07dc9100418223d3005deb1d7f
vaanxy <274824370@qq.com>
2016-09-22 01:05:23 +0800

    part1: my first hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,24 @@ import {Component} from '@angular/core';
 import {CoursesComponent} from './courses.component'
 @Component({
     selector: 'my-app',
-    template: '<h1>我的第一个Angular2应用呀</h1>'
-    // template: '<h1>Hello Angular2</h1><courses></courses>',
-    // directives: [CoursesComponent]
+    template: `<h1>{{title}}</h1>
+                <h2>{{hero.name}} 详情!</h2>
+                <div>
+                    <label>id: </label>{{hero.id}}
+                </div>
+                <div>
+                    <input [(ngModel)]="hero.name" placeholder="给你的英雄起个名字吧！">
+                </div>`
 })
-export class AppComponent { }
+export class AppComponent {
+    title = '英雄编辑器';
+    hero: Hero = {
+        id: 1,
+        name: '暴风城的勇士'
+    };
+}
+
+export class Hero {
+    id: number;
+    name: string;
+}
